4c44c9b9a686ccfca8c12499f3cf2cd4acadd202
Jmoler <167460503+Jmoler@users.noreply.github.com>
2024-05-13 05:21:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#excusa").innerHTML = crearexcusa();
+  console.log("No me lo vas a creer pero...!");
+};
+let crearexcusa = function() {
+  let quien = ["El gobierno", "El perro", "Mi madre", "La inquisición"];
+  let hizo = [" vendio", " regalo", " dono", " quemo"];
+  let aquien = [" el carro", " la camioneta", " la tarea", " el jardin"];
+  let cuando = [
+    " ayer",
+    " el jueves",
+    " hace menos de 5 minutos",
+    " hace 5 horas"
+  ];
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let hizoIndex = Math.floor(Math.random() * hizo.length);
+  let aquienIndex = Math.floor(Math.random() * aquien.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[quienIndex] +
+    hizo[hizoIndex] +
+    aquien[aquienIndex] +
+    cuando[cuandoIndex]
+  );
 };
